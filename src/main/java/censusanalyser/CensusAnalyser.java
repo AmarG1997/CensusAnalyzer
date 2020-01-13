36de2d9187549a3f60b52bbfebaff823ae1a661f
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-13 16:23:51 +0530

    Refactored : Code to follow DRY principle, combine India and Us load method

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -17,21 +17,33 @@ public class CensusAnalyser {
 
     List<IndiaStateCodeDAO> censusStateCodeCSVList = null;
 
-    List<CensusDAO> censusDAO = null;
-
     public CensusAnalyser() {
-        this.censusCSVList = new ArrayList<CensusDAO>();
-        this.censusStateCodeCSVList = new ArrayList<IndiaStateCodeDAO>();
-        this.censusDAO = new ArrayList<CensusDAO>();
+        this.censusCSVList = new ArrayList<>();
+        this.censusStateCodeCSVList = new ArrayList<>();
     }
 
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+            return this.loadCensusData(csvFilePath,IndiaCensusCSV.class);
+    }
+    public int loadUSCensusData(String csvFilePath) throws CensusAnalyserException {
+
+        return this.loadCensusData(csvFilePath,IndiaCensusCSV.class);
+
+    }
+
+    private <E> int loadCensusData(String csvFilePath, Class<E> censusCSVClass) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator <IndiaCensusCSV>csvFileIterator = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
-            while (csvFileIterator.hasNext())
-            {
-                this.censusCSVList.add(new CensusDAO(csvFileIterator.next()));
+            List<E> censusList = csvBuilder.getCSVFileList(reader,censusCSVClass);
+            if (censusCSVClass.getName().equals("censusanalyser.IndiaCensusCSV")){
+                censusList.stream().map(censusCSVClass::cast)
+                        .filter(censusData -> censusCSVList.add(new CensusDAO((IndiaCensusCSV) censusData)))
+                        .collect(Collectors.toList());
+            }
+            else if (censusCSVClass.getName().equals("censusanalyser.USCensus")) {
+                censusList.stream().map(censusCSVClass::cast)
+                        .filter(censusData -> censusCSVList.add(new CensusDAO((USCensus) censusData)))
+                        .collect(Collectors.toList());
             }
             return censusCSVList.size();
 
@@ -44,7 +56,10 @@ public class CensusAnalyser {
         } catch (CsvBuilderException e) {
             throw new CensusAnalyserException(e.getMessage(), e.type.name());
         }
+
     }
+
+
 
     public int loadIndiaStateCodeData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
@@ -63,25 +78,6 @@ public class CensusAnalyser {
         }
     }
 
-    public int loadUSCensusData(String csvFilePath) throws CensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<USCensus> USCensusIterator = csvBuilder.getCSVFileIterator(reader, USCensus.class);
-            while (USCensusIterator.hasNext())
-            {
-                this.censusDAO.add(new CensusDAO(USCensusIterator.next()));
-            }
-            return censusDAO.size();
-        } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        } catch (RuntimeException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.INCORRECT_FILE_DATA);
-        } catch (CsvBuilderException e) {
-            throw new CensusAnalyserException(e.getMessage(), e.type.name());
-        }
-    }
     private <E> int getCount(Iterator<E> censusCSVIterator) {
         Iterable<E> csvIterable = () -> censusCSVIterator;
         int numOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
